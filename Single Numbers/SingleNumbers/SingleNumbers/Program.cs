e3713653fdde1fdd427ce0540df99197ca86aeab
zeinab Mahmoud <71345608+zeynab335@users.noreply.github.com>
2023-02-11 04:15:06 +0200

    add Solution of Single Number Problem

---
+++ Single Numbers/SingleNumbers/SingleNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SingleNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Solution solution = new Solution();
+            Console.WriteLine( solution.SingleNumber(new int[] { 2, 2, 1 }));
+        }
+    }
+}
---
+++ Single Numbers/SingleNumbers/SingleNumbers/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleNumbers
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int DistinctNumbers  = -1;
+
+            // hash set not accept duplicte elements
+            //List<int> Visited = new List<int>();
+            Dictionary<int,int> Visited = new Dictionary<int,int>();
+
+
+            for (int i = 0; i < nums.Length; i++) {
+                
+                if (Visited.ContainsKey(nums[i]))
+                {
+                    Visited[nums[i]] = 1;
+                }
+                else
+                {
+                    Visited[nums[i]] = 0;
+                }
+            }
+            
+            foreach (var value in Visited)
+            {
+                if (value.Value == 0)
+                {
+                    DistinctNumbers = value.Key;
+                }
+            }
+
+
+            return DistinctNumbers;
+
+
+        }
+    }
+}
